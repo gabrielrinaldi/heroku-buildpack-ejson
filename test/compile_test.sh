ce102779e2e0db1dc2055f6304345fd3eaab49fb
Steve Hodgkiss <steve@hodgkiss.me>
2018-10-24 04:57:12 +0000

    Spike: load secrets from a profile.d script on application boot

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -2,15 +2,40 @@
 
 . "$BUILDPACK_TEST_RUNNER_HOME/lib/test_utils".sh
 
-compile_with_fixture() {
+beforeSetUp() {
   TMPDIR=$(mktemp -d)
+}
+
+beforeTearDown() {
+  rm -r "$TMPDIR"
+}
+
+compile_with_fixture() {
   FIXTURE_DIR="$BUILDPACK_HOME/test/fixtures/$1"
   rm -r "$TMPDIR/build" "$TMPDIR/env" "$TMPDIR/cache" 2>/dev/null || true
   cp -r "$FIXTURE_DIR/build" $TMPDIR 2>/dev/null || mkdir "$TMPDIR/build"
   cp -r "$FIXTURE_DIR/cache" $TMPDIR 2>/dev/null || mkdir "$TMPDIR/cache"
   cp -r "$FIXTURE_DIR/env" $TMPDIR 2>/dev/null || mkdir "$TMPDIR/env"
   capture "$BUILDPACK_HOME/bin/compile" "$TMPDIR/build" "$TMPDIR/cache" "$TMPDIR/env"
-  rm -r "$TMPDIR"
+  export_env_dir $1
+}
+
+export_env_dir() {
+  env_dir=$1
+  whitelist_regex=${2:-''}
+  blacklist_regex=${3:-'^(PATH|GIT_DIR|CPATH|CPPATH|LD_PRELOAD|LIBRARY_PATH)$'}
+  FIXTURES_DIR="$BUILDPACK_HOME/test/fixtures/$1/env"
+  echo "ENV: "
+  echo $(ls $FIXTURES_DIR)
+  for e in $(ls $FIXTURES_DIR); do
+    echo "$e" | grep -E "$whitelist_regex" | grep -qvE "$blacklist_regex" &&
+    export "$e=$(cat $BUILDPACK_HOME/test/fixtures/$1/env/$e)"
+    :
+  done
+}
+
+capture_profile_d_export_script() {
+  capture "$TMPDIR/build/.profile.d/export_ejson_secrets.sh"
 }
 
 test_simple() {
@@ -18,76 +43,47 @@ test_simple() {
   assertCapturedSuccess
   assertCaptured "Installing ejson"
   assertCaptured "Loading keypair from environment variables"
-  assertCaptured 'EJSON_ENVIRONMENT is undefined or empty; enumerating and decrypting EJSON files of the form `*.ejson`'
-  assertCaptured "Decrypting config.ejson --> config.json"
-  assertCaptured "Done. Decrypted 1 different ejson file(s)"
+
+  capture_profile_d_export_script
+  assertCapturedSuccess
+  assertCaptured "Decrypting config.ejson"
+  assertCaptured "Done. Decrypted config.ejson"
 }
 
 test_missing_public_key() {
   compile_with_fixture missing_public_key
   assertCapturedError
   assertCaptured "Loading keypair from environment variables"
-  assertCaptured 'EJSON_PUBLIC_KEY is undefined; make sure EJSON_PUBLIC_KEY and EJSON_PRIVATE_KEY are set (try `ejson keygen`)'
+  assertCaptured 'EJSON_PUBLIC_KEY is undefined; make sure EJSON_PUBLIC_KEY and EJSON_PRIVATE_KEY and EJSON_FILE are set'
 }
 
 test_missing_private_key() {
   compile_with_fixture missing_private_key
   assertCapturedError
   assertCaptured "Loading keypair from environment variables"
-  assertCaptured 'EJSON_PRIVATE_KEY is undefined; make sure EJSON_PUBLIC_KEY and EJSON_PRIVATE_KEY are set (try `ejson keygen`)'
+  assertCaptured 'EJSON_PRIVATE_KEY is undefined; make sure EJSON_PUBLIC_KEY and EJSON_PRIVATE_KEY and EJSON_FILE are set'
+}
+
+test_missing_ejson_file() {
+  compile_with_fixture missing_ejson_file
+  assertCapturedError
+  assertCaptured "Loading keypair from environment variables"
+  assertCaptured 'EJSON_FILE is undefined; make sure EJSON_PUBLIC_KEY and EJSON_PRIVATE_KEY and EJSON_FILE are set'
+}
+
+test_ejson_file_not_found_in_build_dir() {
+  compile_with_fixture missing_ejson_file_in_build
+  assertCaptured "Loading keypair from environment variables"
+  capture_profile_d_export_script
+  assertCapturedError
+  assertCaptured "EJSON_FILE could not be found at config.ejson"
 }
 
 test_bad_keypair() {
   compile_with_fixture bad_keypair
-  assertCapturedError
   assertCaptured "Loading keypair from environment variables"
-  assertCaptured "config.ejson: Decryption failed: couldn't read key file"
-}
-
-test_deeply_nested() {
-  compile_with_fixture deeply_nested
-  assertCapturedSuccess
-  assertCaptured "Decrypting foo/bar/baz/launch_codes.ejson --> foo/bar/baz/launch_codes.json"
-  assertCaptured "Done. Decrypted 1 different ejson file(s)"
-}
-
-test_many() {
-  compile_with_fixture many
-  assertCapturedSuccess
-  assertCaptured "Decrypting foo0.ejson --> foo0.json"
-  assertCaptured "Decrypting foo1.ejson --> foo1.json"
-  assertCaptured "Decrypting foo2.ejson --> foo2.json"
-  assertCaptured "Decrypting foo3.ejson --> foo3.json"
-  assertCaptured "Decrypting foo4.ejson --> foo4.json"
-  assertCaptured "Done. Decrypted 5 different ejson file(s)"
-}
-
-test_environment_set() {
-  compile_with_fixture environment_set
-  assertCapturedSuccess
-  assertCaptured 'EJSON_ENVIRONMENT is `production`; enumerating and decrypting EJSON files of the form `*.production.ejson`'
-  assertCaptured 'Decrypting config.production.ejson --> config.json'
-  assertCaptured 'Done. Decrypted 1 different ejson file(s)'
-}
-
-test_environment_unset() {
-  compile_with_fixture environment_unset
-  assertCapturedSuccess
-  assertCaptured 'EJSON_ENVIRONMENT is undefined or empty; enumerating and decrypting EJSON files of the form `*.ejson`'
-  assertCaptured "Decrypting config.ejson --> config.json"
-  assertCaptured "Done. Decrypted 1 different ejson file(s)"
-}
-
-test_overwrite() {
-  compile_with_fixture overwrite
-  assertCapturedSuccess
-  assertCaptured 'EJSON_ENVIRONMENT is `production`; enumerating and decrypting EJSON files of the form `*.production.ejson`'
-  assertCaptured 'Decrypting config.production.ejson --> config.json [overwriting existing file]'
-}
-
-test_overwrite_directory() {
-  compile_with_fixture overwrite_directory
+  capture_profile_d_export_script
+  assertCaptured "Decrypting config.ejson"
   assertCapturedError
-  assertCaptured 'EJSON_ENVIRONMENT is `production`; enumerating and decrypting EJSON files of the form `*.production.ejson`'
-  assertCaptured 'config.json: already exists but is not a regular file'
+  assertCaptured "Decryption failed: couldn't decrypt message"
 }
